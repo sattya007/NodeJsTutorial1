7bd6bcb1baa78b27b8df1802b206b2f5b1d1725a
sattya007 <109289722+sattya007@users.noreply.github.com>
2024-01-22 01:04:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,136 @@
+//console.log("Welcome to hello world!");
+//console.log("We are JS");
+//FullName = "Tony Stark";
+//age=24;
+//x=null;
+//y=undefined;
+//isFollow=true;
+//FullName=25
+//console.log(isFollow);
+//console.log(FullName);
+//let FullName="Shark";
+//console.log(FullName);
+//let Age=24;
+//let totalPrice=1000;
+//console.log(totalPrice); 
+//var example="Rahim";
+//var example="Ram";
+//console.log(example);
+// {
+//     let a=5;
+//     console.log(a);
+// }
+// {
+//     let a=10;
+//     console.log(a);
+// }
+//let x=BigInt("123");
+//console.log(x);
+// const student = {
+//     FullName : "Sattya",
+//     age : 20,
+//     cgpa : 8.2,
+// };
+//student.age=student.age + 1;
+//console.log(student.age);
+// student.FullName="Sayan";
+// console.log(student.FullName);
+// const product = {
+//     title : "Ball Pen",
+//     rating : 4,
+//     offer : 5,
+//     price : 270,
+// };
+//console.log(typeof product.title);
+//let a=5;
+//let b=2;
+//let c="2";
+//console.log("a-- =" ,a--);
+//console.log("a = ",a);
+//console.log("a + b = ",a+b);
+//a **=4;
+//console.log("a ^ a = ",a);
+//console.log("5 != 2",a!=b);
+//console.log("b !==c", b!==c);
+//console.log("!(a>b) = ",!(a>b));
+// if(a%2===0)
+// {
+//     console.log(a,"is even");
+// }
+// else
+// {
+//     console.log(a,"is odd");
+// }
+//let age=20;
+//let res=age >= 18 ? "adult" : "not adult";
+//console.log(res);
+// const expr = "papayas";
+// switch(expr){
+//     case "org":
+//         console.log("Org costs 6 rupees!");
+//         break;
+//     case "papayas":
+//         console.log("papayas are 10 rupees");
+//         break;
+//     default:
+//         console.log(`sorry , we are out of ${expr}`);        
+// }
+//let fullName=prompt("hello!");
+//console.log(fullName);
+// let num=prompt("enter the number:::");
+// if(num % 5 === 0)
+// {
+//     console.log(num,"it is divisible of 5");
+// }
+// else
+// {
+//     console.log("it is not divisible of 5");
+// }
+// let score=prompt("Enter your choice from (0 to 100)");
+// let grade;
+// if(score >= 90 && score <= 100)
+// {
+//     grade="A";
+// }else if(score >=70 && score <=89)
+// {
+//     grade="B";
+// }
+// else if(score >=60 && score <=69)
+// {
+//     grade="C";
+// }
+// else if(score>=50 && score<=59)
+// {
+//     grade="D";
+// }
+// else if(score>=0 && score<=49)
+// {
+//     grade="F";
+// }
+// console.log("Accroding to your scores , your grade is:: ",grade);
+// for(let count=1;count<=4;count++)
+// {
+//     console.log("Sattya!");
+// }
+// let count=0;
+// while(count<=10)
+// {
+//     console.log(count);
+//     count++;
+// }
+// for(let val of strVar)
+// {
+    
+// }
+// let flag=false;
+// if(flag){
+//     console.log(flag);
+// }
+// else{
+//     console.log(flag);
+// }
+const sayHello = (name,greeting) => {
+    console.log("Hello" + name + " " + greeting);
+}
+sayHello(" Sattya","Good Afternoon");
+
